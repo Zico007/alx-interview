bd807479decf10dec9db215f7d02d2b536d5d5f2
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2023-10-05 14:58:34 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains functions to generate and print Pascal's triangle.
+"""
+
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n."""
     if n <= 0:
