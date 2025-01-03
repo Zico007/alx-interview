ad809a2e337ee5de68e2869b4dacea3a6b6ed288
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2025-01-03 06:00:31 -0500

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,15 +2,12 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
-    triangle = [[1]]  
-
-    for i in range(1, n):
+    triangle = []
+    for i in range(n):
         row = [1]
-        for j in range(1, i):
-            
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([last_row[j] + last_row[j + 1] for j in range(len(last_row) - 1)])
+            row.append(1)
         triangle.append(row)
-    
     return triangle
