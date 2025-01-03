8d800841f8940b511f24f84dde9302bedc017bde
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2025-01-03 06:09:54 -0500

    Update 0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
         row = [1]
         if triangle:
             last_row = triangle[-1]
-            row.extend([last_row[j] + last_row[j + 1] for j in range(len(last_row) - 1)])
+            row += [last_row[j] + last_row[j + 1] for j in range(len(last_row) - 1)]
             row.append(1)
         triangle.append(row)
     return triangle 
